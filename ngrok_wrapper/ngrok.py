ce5ff5a210c4339b01089702bb447c90f474d280
Ramchandra <thakuri.ram12@gmail.com>
2023-01-02 01:25:20 +0545

    task/2- Defined `poll_tunnels` method and code refactor
    -- Build `poll_tunnels` to poll the tunnels and regenerate the `default_tunnel` when it is not found in `ngrok_get_tunnels`
    -- Defined `static` utility methods i.e `get_public_urls` and `select_https_url`
    -- Code refactor mainly for thread management

--- ngrok_wrapper/ngrok.py
+++ ngrok_wrapper/ngrok.py
@@ -1,5 +1,8 @@
-import threading
+import logging
 from pyngrok import ngrok
+from utility.custom_thread import CustomThread
+from pyngrok.exception import PyngrokNgrokURLError, PyngrokNgrokURLError
+
 
 
 class TunnelNg:
@@ -8,54 +11,60 @@ class TunnelNg:
         self.auth_token = auth_token
         self.request_type = request_type
         ngrok.set_auth_token(self.auth_token)
-        self.tunnel_runner = AppRunner(self.ngrok_process)
+        self.default_tunnel = None
+        self.tunnel_runner = None
+        self.tunnel_process_runner = None
+        self.start_tunnel()
 
     def __del__(self):
         self.stop_tunnel()
 
-    def connect(self):
-        #self.tunnel_runner.join()
-        ngrok.connect(self.port, self.request_type)
-        self.start_tunnel()
-        return ngrok.get_tunnels()[1].public_url
-
+    def build_tunnel_threads(self):
+        self.tunnel_runner = CustomThread(self.ngrok_process)
+        self.tunnel_process_runner = CustomThread(ngrok.get_ngrok_process().proc.wait)
+        
     def start_tunnel(self):
+        while not self.tunnel_runner\
+            or not self.tunnel_process_runner:
+            self.build_tunnel_threads()
+        ngrok.connect(self.port, self.request_type)
+        self.default_tunnel =  self.select_https_url(self.get_public_urls(ngrok.get_tunnels))
+        logging.warning('Default tunnel updated: %s', self.default_tunnel) # TODO: Custom logging w/ color 
         self.tunnel_runner.start()
 
+
     def stop_tunnel(self):
-        self.tunnel_runner.stop()
+        ngrok.kill()
+        self.tunnel_runner.stop_it() 
+        self.tunnel_process_runner.stop_it()
         self.tunnel_runner.join()
+        self.tunnel_process_runner.join()
+
+        self.tunnel_runner = None
+        self.tunnel_process_runner  = None
 
     def ngrok_process(self):
-        process_runner = AppRunner(ngrok.get_ngrok_process().proc.wait)
         try:
-            process_runner.start()
+            self.tunnel_process_runner.start()
         except KeyboardInterrupt:
-            ngrok.kill()
-            process_runner.join()
-            process_runner.stop()
-            self.tunnel_runner.stop()
+            self.stop_tunnel()
 
-
-
-class AppRunner(threading.Thread):
-    def __init__(self, target):
-        super().__init__()
-        self.target = target
-        self._stop = threading.Event()
-
-    def run(self):
-        self.target()
+    def poll_tunnels(self):
         while True:
-            print('run')
-            if self.stopped():
-                return
+            try:
+                if self.default_tunnel  not in self.get_public_urls(ngrok.get_tunnels):
+                    self.stop_tunnel()
+                    self.start_tunnel()
+            # TODO: Add possible exceptions and handle it
+            except [ConnectionResetError, PyngrokNgrokURLError, PyngrokNgrokURLError]: 
+                pass
 
-    def stop(self):
-        self._stop.set()
 
-    def stopped(self):
-        return self._stop.is_set()
+    @staticmethod
+    def get_public_urls(ngrok_get_tunnel_callback) -> list:
+        return [tunnel.public_url for tunnel in ngrok_get_tunnel_callback()]
 
-check = TunnelNg('2JiTOdikcjiu8PfbBz07obIgx4a_4DAVcpvzBUQ3eRmryfB23',40,"http")
-print(check.connect())
+    @staticmethod
+    def select_https_url(urls_list) -> str or None:
+        https_url = list(filter(lambda x : x.startswith('https'), urls_list))
+        return len(https_url) and https_url[0] or None
