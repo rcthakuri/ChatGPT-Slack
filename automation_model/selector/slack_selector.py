187be0046d6bf5b5cf84718a67ba261f69133a4b
anjanthapa26 <thapaanjan40@gmail.com>
2023-01-02 14:32:14 +0545

    task/3- Defined `Selector` class for slack automation
    -- Exposed the selector through the
        selector class method i.e
        `get_selector_name`

---
+++ automation_model/selector/slack_selector.py
@@ -0,0 +1,67 @@
+# URL - url based selector 
+LOGIN_URL = "https://team-next-world.slack.com/sign_in_with_password"
+SUBSCRIPTIONS_URL = "https://api.slack.com/apps/A04HF34TGHF/event-subscriptions?"
+
+# XPATH - xpath based selector for slack event subscription
+USERNAME = "//input[@id='email']"
+PASSWORD = "//input[@id='password']"
+LOGIN_BUTTON = "//button[normalize-space()='Sign In']"
+SEND_KEYS_TO_REQUEST_URL = "//input[@id='request_url']"
+CHANGE_REQUEST_URL_BUTTON = "//button[@id='change_request_url']"
+SAVE_CHANGES_BUTTON = "//button[@data-qa='save_changes_button']"
+
+
+class Selector:
+    def __init__(self):
+        self.event_subscription_selector = EventSubscriptionSelector()
+
+
+
+class EventSubscriptionSelector:
+
+    def __init__(self):
+        self.__EventSubscriptionSelector = {}
+        self.init_event_subscription_selector()
+
+    def init_event_subscription_selector(self):
+        self.__EventSubscriptionSelector['username'] = USERNAME
+        self.__EventSubscriptionSelector['password'] = PASSWORD
+        self.__EventSubscriptionSelector['login_url'] = LOGIN_URL
+        self.__EventSubscriptionSelector['login_button'] = LOGIN_BUTTON
+        self.__EventSubscriptionSelector['subscription_url'] = SUBSCRIPTIONS_URL
+        self.__EventSubscriptionSelector['save_changes_button'] = SAVE_CHANGES_BUTTON
+        self.__EventSubscriptionSelector['change_request_url_button'] = CHANGE_REQUEST_URL_BUTTON
+        self.__EventSubscriptionSelector['send_keys_to_request_url'] = SEND_KEYS_TO_REQUEST_URL
+
+
+    def get_username(self) -> str:
+        return self.__EventSubscriptionSelector['username']
+
+    def get_password(self) -> str:
+        return self.__EventSubscriptionSelector['password']
+
+    def get_login_url(self) -> str:
+        return self.__EventSubscriptionSelector['login_url']
+
+    def get_login_button(self) -> str:
+        return self.__EventSubscriptionSelector['login-button']
+
+    def get_subscription_url(self) -> str:
+        return self.__EventSubscriptionSelector['subscription_url']
+
+    def get_save_changes_button(self) -> str:
+        return self.__EventSubscriptionSelector['save_changes_button'] 
+
+    def get_send_keys_to_request_url(self) -> str:
+        return self.__EventSubscriptionSelector['send_keys_to_request_url']
+
+    def get_change_request_url_button(self) -> str:
+        return self.__EventSubscriptionSelector['change_request_url_button']
+
+
+
+
+
+    
+
+    
