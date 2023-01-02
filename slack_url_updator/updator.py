616e2d0d86af481516aed05a12121f2be0aac9bf
Ramchandra <thakuri.ram12@gmail.com>
2023-01-02 21:36:30 +0545

    PEP8

--- slack_url_updator/updator.py
+++ slack_url_updator/updator.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 from ngrok_wrapper.ngrok import TunnelNg
 from automation.automate.automate_slack_event_subscription import SlackUrlUpdater
 
-
 load_dotenv()
 
 # SLACK CREDENTIAL 
@@ -14,6 +13,7 @@ SLACK_USERNAME = os.environ.get("SLACK_USERNAME")
 NGROK_PORT = os.environ.get('NGROK_PORT')
 NGROK_AUTH_TOKEN = os.environ.get('NGROK_AUTH_TOKEN')
 NGROK_REQUEST_TYPE = os.environ.get('NGROK_REQUEST_TYPE')
+
 
 def updator():
     slack_cred = {
@@ -30,6 +30,6 @@ def updator():
     slack_url_updator = SlackUrlUpdater(slack_cred)
     TunnelNg(ngrok_key, slack_url_updator.update_request_url).poll_tunnels()
 
-    
+
 if __name__ == "__main__":
     updator()
